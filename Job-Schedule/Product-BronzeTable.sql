c30de2d10061067bda87a1cd0deb44520ab97d5d
inf7m <dpnt1510@gmail.com>
2023-08-01 22:39:46 +0700

    Future Using BRONZE-SILVER-GOLD TABLE

---
+++ Job-Schedule/Product-BronzeTable.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE BronzeFashionProduct (
+    id PRIMARY KEY,
+    gender VARCHAR,
+    masterCategory VARCHAR,
+    subCategory VARCHAR,
+    articleType VARCHAR,
+    baseColour VARCHAR,
+    season VARCHAR,
+    year NUMERIC,
+    usage VARCHAR,
+    productDisplayName VARCHAR
+)
+
