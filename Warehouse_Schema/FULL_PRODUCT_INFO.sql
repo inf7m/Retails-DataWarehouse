5df5d7131d5ba585a6567aa8d95e718b9d77fa70
inf7m <dpnt1510@gmail.com>
2023-08-01 22:37:39 +0700

    LOAD Product-masterfile.csv into a table before load into ProductDimensionTable

---
+++ Warehouse_Schema/FULL_PRODUCT_INFO.sql
@@ -0,0 +1,13 @@
+CREATE TABLE FULL_PRODUCT_INFO
+(
+    id                 NUMBER,
+    gender             VARCHAR2(50),
+    masterCategory     VARCHAR2(50),
+    subCategory        VARCHAR2(50),
+    articleType        VARCHAR2(50),
+    baseColour         VARCHAR2(50),
+    season             VARCHAR2(50),
+    year               NUMBER,
+    usage              VARCHAR2(50),
+    productDisplayName VARCHAR2(100)
+)
