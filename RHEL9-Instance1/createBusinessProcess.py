0c63485464e4a4adde33ab1738de73b510911504
inf7m <dpnt1510@gmail.com>
2023-08-05 16:34:09 +0700

    Update the script for Business Operator!

--- RHEL9-Instance1/createBusinessProcess.py
+++ RHEL9-Instance1/createBusinessProcess.py
@@ -2,28 +2,36 @@ import datetime
 import csv
 import configparser
 import random
+
 configuration = configparser.ConfigParser
 csvWriter = csv.writer()
+
+
 def getCurrentDate() -> datetime.date:
-    return datetime.date.today() # YYYY-MM-DD
+    return datetime.date.today()  # YYYY-MM-DD
+
 
 def outputFilePath() -> str:
-    stringFilePath = "BusinessProcess-"+str(getCurrentDate())
+    stringFilePath = "/BusinessProcess/BusinessProcess-" + str(getCurrentDate())+'.csv'
     return stringFilePath
 
-csvWriter = csv.writer(outputFilePath(),'a')
 
-
+csvWriter = csv.writer(outputFilePath(), 'a')
 
 
 def createBusinessProcess() -> list:
-    saleQuantity = random.randint(1,10)
-    regularPrice = random.randint(100,499)
-    discountUnitPrice = random.randint(1,20)
-    netUnitPrice = random.randint(40,99)
+    saleQuantity = random.randint(1, 10)
+    regularPrice = random.randint(100, 499)
+    discountUnitPrice = random.randint(1, 20)
+    netUnitPrice = random.randint(40, 99)
     # Extended things : 4 -> default as 0
-    resultRows = list(saleQuantity,regularPrice,discountUnitPrice,netUnitPrice,0,0,0,0, str(datetime.date.today()),)
-    return
+
+    resultRows = [saleQuantity, regularPrice, discountUnitPrice, netUnitPrice, 0, 0, 0, 0,
+                  str(datetime.date.today()), random.randint(1, 100), random.randint(1, 100),
+                  random.randint(0, 44445), random.randint(209, 334), random.randint(1, 5)]
+    return resultRows
+
 
 with open(outputFilePath(), 'a') as BusinessProcess_Output:
-        csvWriter.writerow()
+    for i in range(1, 2000):
+        csvWriter.writerow(createBusinessProcess())
